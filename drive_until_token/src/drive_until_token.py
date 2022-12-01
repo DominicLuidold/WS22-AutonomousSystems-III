52c8bde36af6965de75439cbe0d9878ed8763b19
Dominic Luidold <dominic.luidold@students.fhv.at>
2022-12-01 16:43:57 +0100

    Fix a bit, make a lot not working too

--- drive_until_token/src/drive_until_token.py
+++ drive_until_token/src/drive_until_token.py
@@ -1,51 +1,74 @@
+#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
 from pixy_msgs.msg import PixyData
 
 class DriveUntilToken:
-    def __init__(self) -> None:
-        rospy.init_node('turtlebot3_drive_until_token', anonymous=False)
-        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        rospy.loginfo("Initialized")
-        self.behaviors = [DriveAndSearch(), FoundTag()]
-        self.currBehavior = 0
+    def __init__(self):
+        # Init
+        rospy.loginfo("Initialized!")
+        
+        # Behaviors
+        self._behaviors = [DriveAndSearch(), FoundTag()]
+        self._currBehavior = 0
+
+        # Publish
+        self._cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self._rate = rospy.Rate(10)
+
+        # Start
         self.process()
 
     def process(self):
-        (linear, angular, changeBehavior) = self.behaviors[self.currBehavior]
-        tw = Twist()
-        tw.linear.x = linear
-        tw.angular.z = angular
-        self.currBehavior += changeBehavior
-        self.pub.publish(tw)
+        (linear, angular, changeBehavior) = self._behaviors[self._currBehavior].process()
+        rospy.loginfo(self._behaviors[self._currBehavior])
+        
+        twist = Twist()
+        twist.linear.x = linear
+        twist.angular.z = angular
+        
+        self._currBehavior += changeBehavior
+        self._cmd_pub.publish(twist)
 
 class DriveAndSearch:
-    def __init__(self) -> None:
-        self.process()
+    def __init__(self):
+        # Init 
+        rospy.loginfo("Drive and Search")
+
+        # Subscribe
+        self._cmd_sub = rospy.Subscriber('/my_pixy/block_data', PixyData)
 
     def process(self):
-        rospy.loginfo("drive and search")
-        if self.found_tag() :
-            return 0, 0, 1
+        if self.found_tag():
+            return (0.2, 0, 1)
         else:
-            return 1, 0, 0
+            return (0, 0.2, 0)
 
-    def found_tag(self):
-        sub = rospy.Subscriber('/my_pixy/block_data', data_class=PixyData)
-        rospy.loginfo(sub)
+    def found_tag(self) -> bool:
+        rospy.loginfo(self._cmd_sub)
+
         return False
 
 class FoundTag:
-    def __init__(self) -> None:
-        self.process()
+    def __init__(self):
+        pass
 
     def process(self):
         rospy.loginfo("found tag")
         rospy.loginfo("loud noise")
 
-if __name__ == '__main__':
+        return (1, 1, 0)
+
+def main():
+    # Init
+    rospy.init_node('turtlebot3_drive_until_token')
+
+    # Start
     try:
         DriveUntilToken()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
+
+if __name__ == '__main__':
+    main()
