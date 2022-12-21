cc69591044d2dedba6b0ed7c4ae66b5070fa32c3
Florian Bereuter <florian.bereuter@hotmail.com>
2022-12-21 16:00:31 +0100

    publish movements towards token WIP

--- token_detector/src/token_detector.py
+++ token_detector/src/token_detector.py
@@ -10,30 +10,55 @@ import numpy as np
 import math
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
 
-class token_detector:
+class Turtle:
   def __init__(self):
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/raspicam_node/image/compressed",CompressedImage,self.detect_token)
-    self.tokens = []
-    #self.raspicam_base = np.array([640, 960])
+    rospy.init_node('token_detector', anonymous=True)
+    self.__pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    self.__behaviors = [TargetReached(), MoveTowardsToken()]
+    self.__token_detector = TokenDetector()
+
+  def keep_movin(self):
+    while not rospy.is_shutdown():
+      if len(self.__token_detector.tokens):
+        for b in self.__behaviors:
+          if b.isApplicable(self.__token_detector.tokens):
+            b.execute(self, self.__token_detector.tokens)
+            break
+      else:
+        self.move(0,0)
+      rospy.Rate(10).sleep()
+  
+
+  def move(self, linear, angular):
+    move = Twist()
+    move.linear.x = linear
+    move.angular.z = angular
+    print(f'pub {move.linear.x} {move.angular.z}')
+    self.__pub.publish(move)
+
+
+class TokenDetector:
+  def __init__(self):
+    self.__bridge = CvBridge()
+    self.__image_sub = rospy.Subscriber("/raspicam_node/image/compressed",CompressedImage,self.detect_token)
+    self.tokens = [] # [x,y,y-distance[%],angle[rad]]
 
   def detect_token(self,data):
     try:
-      cv_image_bgr = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
+      cv_image_bgr = self.__bridge.compressed_imgmsg_to_cv2(data, "bgr8")
       self.raspicam_base = np.array([cv_image_bgr.shape[1]//2, cv_image_bgr.shape[0]])
       mask = self.get_token_mask(cv_image_bgr)
       mask = self.remove_noise(mask)
       contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
       if contours:
         self.tokens = self.calculate_token_centers(contours)
-        sorted_tokens_closest = self.sort_tokens_by_distance()
-        angle = self.calculate_angle_of_closest(sorted_tokens_closest)
-
+        self.sort_tokens_by_distance()
+        self.set_angle_of_tokens()
         #self.print_contours(mask, contours)
       else:
         self.tokens = []
-      
     except CvBridgeError as e:
       print(e)
   
@@ -58,29 +83,31 @@ class token_detector:
     closed = cv.morphologyEx(opened, cv.MORPH_CLOSE, kernel)
     return closed
 
+
   def calculate_token_centers(self, contours):
     """
-    TODO improve accuracy
+    TODO improve accuracy by either just using left/rightmost bzw hightst/lowest points middle of contour or approximating down to 4 points like in ba2
     """
-    return np.array([cv.convexHull(contour).mean(axis=0, dtype=np.int32)[0] for contour in contours])
+    return np.array([np.append(np.array(cv.convexHull(contour).mean(axis=0, dtype=np.int32)[0]), np.zeros(2), axis=0) for contour in contours])
+
 
   def sort_tokens_by_distance(self):
     """
     sort only by difference on y-axis as image is distorted
     """
-    #distances = [np.linalg.norm(self.raspicam_base - token) for token in self.tokens]
     distances = [self.raspicam_base[1] - token[1] for token in self.tokens]
+    for i in range(len(self.tokens)):
+      self.tokens[i][2] = distances[i] / self.raspicam_base[1]
     indexes = np.argsort(distances)
-    return self.tokens[indexes]
+    self.tokens = self.tokens[indexes]
 
-
-  def calculate_angle_of_closest(self, tokens):
+  def set_angle_of_tokens(self):
     """
     Calculate the angle in radians along the y-axis! -> Directly in front = 0, slightly to the right is -0.x, left = +0.x
     """
-    closest_token = tokens[0]
-    angle = math.atan2(self.raspicam_base[0] - closest_token[0], self.raspicam_base[1] - closest_token[1])
-    return angle
+    for token in self.tokens:
+      token[3] = math.atan2(self.raspicam_base[0] - token[0], self.raspicam_base[1] - token[1]) # atan2(phi) = gk/ak = x-x/y-y
+
 
   def print_contours(self, mask, contours):
     if contours:
@@ -97,9 +124,54 @@ class token_detector:
 
 
 
+class TargetReached:
+  """
+  Time to celebrate!
+  """
+  def isApplicable(self, tokens):
+    """
+    if scan and scan.ranges[0] < target_reached_dist:
+      return True
+    return False
+    """
+    return False
+
+  def execute(self, token_detector):
+    print('Target reached!!!')
+    #token_detector.publish_move(0,0)
+
+
+class MoveTowardsToken:
+  """
+  Target is ahead and in sight
+  """
+  def isApplicable(self, tokens):
+    return True
+
+  def execute(self, killerrobot, tokens):
+    clst_tkn = tokens[0]
+    linear_velocity = 0.26 * clst_tkn[2] / 2
+    angular_velocity = clst_tkn[3] * 0.3
+    killerrobot.move(linear_velocity, angular_velocity)
+    """
+    closest_dist = min(turtle.scan.ranges)
+    closest_index = turtle.scan.ranges.index(closest_dist)
+    distance_percent = min((closest_dist - min_detection_dist) / (0.5 - min_detection_dist), 1)
+    linear_velocity = max_speed * distance_percent
+    angular_velocity = 0
+    if closest_index < 180: # target is on left side -> turn left
+      angular_velocity = closest_index / 20 * 0.5
+    else:
+      angular_velocity = (360 - closest_index) / 20 * -0.5
+    print('towards linear {} angular {} min distance {} closest index {}'.format(linear_velocity, angular_velocity, closest_dist, closest_index))
+    turtle.publish_move(linear_velocity, angular_velocity)
+    """
+    pass
+
+
 def main(args):
-  token_detector()
-  rospy.init_node('token_detector', anonymous=True)
+  tb = Turtle()
+  tb.keep_movin()
   try:
     rospy.spin()
   except KeyboardInterrupt:
