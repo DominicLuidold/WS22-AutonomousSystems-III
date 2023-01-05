4e769baa443e67b5feb263cd8bbc3204538c6652
Florian Bereuter <florian.bereuter@hotmail.com>
2023-01-05 16:19:27 +0100

    show also simulated image in image_viewer

--- token_detector/src/image_viewer.py
+++ token_detector/src/image_viewer.py
@@ -6,17 +6,26 @@ roslib.load_manifest('token_detector')
 import sys
 import rospy
 import cv2
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image
 from cv_bridge import CvBridge, CvBridgeError
 
 class image_viewer:
   def __init__(self):
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/raspicam_node/image/compressed",CompressedImage,self.callback)
+    self.image_sub = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.callback)
+    self.image_sub2 = rospy.Subscriber("/camera/rgb/image_raw", Image, self.simulated_callback)
 
   def callback(self,data):
     try:
       cv_image = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
+      cv2.imshow("Image window", cv_image)
+      cv2.waitKey(3)
+    except CvBridgeError as e:
+      print(e)
+  
+  def simulated_callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
       cv2.imshow("Image window", cv_image)
       cv2.waitKey(3)
     except CvBridgeError as e:
