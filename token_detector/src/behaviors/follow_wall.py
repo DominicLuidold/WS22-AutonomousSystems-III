18c125dfae8aa37ee65f8cf6d621c121b5e3fc06
Florian Bereuter <florian.bereuter@hotmail.com>
2023-02-03 13:39:44 +0100

    fix wall_followers in follow_wall and move_towards_token

--- token_detector/src/behaviors/follow_wall.py
+++ token_detector/src/behaviors/follow_wall.py
@@ -21,13 +21,12 @@ class WallFollower:
   """ Follow wall on the left side """
   
   def __init__(self, killerrobot) -> None:
-    self.__killerrobot = killerrobot
+    self._killerrobot = killerrobot
     self._behaviors = [CompleteRoundtrip(), TurnTowardsWall(), FollowWall()]
     self._scan = None
     self._odom = None
     self._started = False
-
-    # Subscribed topics
+    self.dist = None
     rospy.Subscriber('scan', LaserScan, self.__process_scan)
     rospy.Subscriber('odom', Odometry, self.__process_odom)
 
@@ -36,37 +35,39 @@ class WallFollower:
 
   def __process_odom(self, data: Odometry) -> None:
     self._odom = data
-
-    # Check only as long as it is initially in the starting area
-    if not self._started:
+    if not self._started: # Check only as long as it is initially in the starting area
       self._started = not isNearStart(data.pose.pose.position.x, data.pose.pose.position.y, MAX_TARGET_DISTANCE + 0.05)
-      #rospy.logdebug("Leaving start area...")
 
   def isApplicable(self) -> bool:
-    """ if wall is somewhere at front """
-    return any([b.isApplicable(self._scan, self._odom, self._started, self.__killerrobot) for b in self._behaviors])
+    """ if wall is somewhere at front - left """
+    if self._scan:
+      ranges = self._scan.ranges
+      range_min = self._scan.range_min
+      self.dist = {'front': filtered_min(ranges[:15] + ranges[-15:], range_min), \
+                   'front_left': filtered_min(ranges[30:60], range_min), \
+                   'left': filtered_min(ranges[75:105], range_min)}
+      return any([b.isApplicable(self.dist, self._odom, self._started, self._killerrobot) for b in self._behaviors])
+    return False
 
   def execute(self) -> None:
-    rospy.logdebug('behavior: follow wall')
+    rospy.logwarn('behavior: follow wall')
     for b in self._behaviors:
-      if b.isApplicable(self._scan, self._odom, self._started, self.__killerrobot):
-        b.execute(self._scan, self.publish_move, self.__killerrobot)
+      if b.isApplicable(self.dist, self._odom, self._started, self._killerrobot):
+        b.execute(self.dist, self.publish_move, self._killerrobot)
         break
 
   def publish_move(self, linear: float, angular: float) -> None:
-    self.__killerrobot.move(linear, angular)
+    self._killerrobot.move(linear, angular)
 
 
 class CompleteRoundtrip:
-  def isApplicable(self, scan: LaserScan, odom: Odometry, started: bool, killerrobot) -> None:
+  def isApplicable(self, distances, odom: Odometry, started: bool, killerrobot) -> None:
     # Check when odom value is entered and if it has left the starting area
     if odom is not None and started:
         rospy.logdebug("Check: x: " + str(odom.pose.pose.position.x) + " , y: " + str(odom.pose.pose.position.y))
 
         #check if turtlebot has reached its initial starting position
         if isNearStart(odom.pose.pose.position.x, odom.pose.pose.position.y, MAX_TARGET_DISTANCE):
-          
-          rospy.logerr(killerrobot.map_saved)
           if not killerrobot.map_saved:
             return True
           else:
@@ -75,7 +76,7 @@ class CompleteRoundtrip:
 
     return False
 
-  def execute(self, scan: LaserScan, publish_move, killerrobot) -> None:
+  def execute(self, distances, publish_move, killerrobot) -> None:
       if not killerrobot.map_saved:
         rospy.loginfo("Saving map!")
         os.system("rosrun map_server map_saver -f /killerrobot/saved-map")
@@ -83,26 +84,16 @@ class CompleteRoundtrip:
       rospy.loginfo("Finished")
 
 class TurnTowardsWall:
-  def isApplicable(self, scan: LaserScan, odom: Odometry, started: bool, killerrobot) -> bool:
+  def isApplicable(self, dist, odom: Odometry, started: bool, killerrobot) -> bool:
     """
     Applicable as soon as the wall bends away from the turtle (it moved past the corner)
     Stops and turns left.
     """
-    if scan:
-      ranges = scan.ranges
-      range_min = scan.range_min
-      # order of sensors: front, front_left, left, behind_left
-      dist = [filtered_min(ranges[:15] + ranges[-15:], range_min), filtered_min(ranges[30:60], range_min), filtered_min(ranges[75:105], range_min), filtered_min(ranges[120:150], range_min)]
-      if dist[0] > MAX_DETECTION_DIST and dist[1] > MAX_DETECTION_DIST and dist[2] <= MAX_DETECTION_DIST:
-        return True
-    return False
+    return dist['front'] > MAX_DETECTION_DIST and dist['front_left'] > MAX_DETECTION_DIST and dist['left'] <= MAX_DETECTION_DIST
 
-  def execute(self, scan: LaserScan, publish_move, killerrobot) -> None:
-    ranges = scan.ranges
-    range_min = scan.range_min
-    dist = [filtered_min(ranges[:15] + ranges[-15:], range_min), filtered_min(ranges[30:60], range_min), filtered_min(ranges[75:105], range_min), filtered_min(ranges[120:150], range_min)]
+  def execute(self, dist, publish_move, killerrobot) -> None:
     angular_k = 1 # the further away, the sharper the turn
-    closeness_percent = (1 - (dist[2] - MIN_DETECTION_DIST) / (MAX_DETECTION_DIST - MIN_DETECTION_DIST))
+    closeness_percent = (1 - (dist['left'] - MIN_DETECTION_DIST) / (MAX_DETECTION_DIST - MIN_DETECTION_DIST))
     linear_velocity = MAX_SPEED * closeness_percent # the further away, the slower
     angular_velocity = closeness_percent
     rospy.logdebug('turn linear {} angular {} sensors {}'.format(linear_velocity, angular_velocity, dist))
@@ -115,21 +106,16 @@ class FollowWall:
   Takes into account the sensors in front, left front and on the left hand side
   and calculates the linear and angular velocity.
   """
-  def isApplicable(self, scan: LaserScan, odom: Odometry, started: bool, killerrobot) -> bool:
-    if scan and any(item < MAX_DETECTION_DIST for item in scan.ranges):
-      return True
-    return False
+  def isApplicable(self, dist, odom: Odometry, started: bool, killerrobot) -> bool:
+    return any(distance < MAX_DETECTION_DIST for distance in dist.values())
 
-  def execute(self, scan: LaserScan, publish_move, killerrobot) -> None:
-    ranges = scan.ranges
-    range_min = scan.range_min
-    # order of sensors: front, front_left, left
-    dist = [filtered_min(ranges[:15] + ranges[-15:], range_min), filtered_min(ranges[30:60], range_min), filtered_min(ranges[75:105], range_min)]
+  def execute(self, distance, publish_move, killerrobot) -> None:
     linear_k = [1.5 * MAX_SPEED, 0, 0]
-    angular_k = [-1, -0.3, 0.2]
+    angular_k = [-1, -0.4, 0.2]
     sensitivity_dist = [1.5 * MAX_DETECTION_DIST, MAX_DETECTION_DIST, MAX_DETECTION_DIST]
     linear_velocity = MAX_SPEED
     angular_velocity = 0
+    dist = [distance['front'], distance['front_left'], distance['left']]
     for i in range(3):
       if dist[i] <= sensitivity_dist[i]:
         linear_velocity -= linear_k[i] * (1 - (dist[i] - MIN_DETECTION_DIST) / (sensitivity_dist[i] - MIN_DETECTION_DIST))
