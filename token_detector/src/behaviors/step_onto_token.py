deaf8373ef48fd336c6c837f9326397c82bdb223
Florian Bereuter <florian.bereuter@hotmail.com>
2023-02-03 15:15:01 +0100

    fix isovertoken to enter capture_token behavior

--- token_detector/src/behaviors/step_onto_token.py
+++ token_detector/src/behaviors/step_onto_token.py
@@ -6,17 +6,17 @@ class StepOntoToken:
   """ Use pixycam to position above token """
 
   def __init__(self, killerrobot):
-    self.__pixycam_detector = PixycamDetector()
+    self._pixycam_detector = PixycamDetector()
     self.position = (0,0)
-    self.__killerrobot = killerrobot
+    self._killerrobot = killerrobot
 
   def isApplicable(self) -> bool:
-    return self.__pixycam_detector.is_detecting_token or \
-           self.__pixycam_detector.consecutive_non_detections < 5
+    return self._pixycam_detector.is_detecting_token or \
+           self._pixycam_detector.consecutive_non_detections < 5
              # there are always non-detections inbetween even if it should detect a token (only god knows why)
 
   def execute(self) -> None:
     """ position above token """
-    rospy.loginfo('behavior: step onto token')
-    self.__killerrobot.move(self.__killerrobot.max_speed / 7, 0)
-    self.__killerrobot._isovertoken = True
+    rospy.logwarn('behavior: step onto token')
+    self._killerrobot.move(self._killerrobot.max_speed / 7, 0)
+    self._killerrobot.isovertoken = True
